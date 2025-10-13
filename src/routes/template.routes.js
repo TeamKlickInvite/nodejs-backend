887454630484dfa9d445f9f344cd0a9e18f09b5f
dev <dev@klickinvite.com>
2025-10-13 14:17:10 +0530

    MOVE GUEST TO ANOTHER GROUP

--- src/routes/template.routes.js
+++ src/routes/template.routes.js
@@ -28,8 +28,7 @@ router.delete('/deleteGuest/:guest_id',checkFrappeAuth,deleteGuest)
 router.put('/updateGuest/:guest_id',checkFrappeAuth,updateGuest)
 router.get("/getGuestsByHost/:host_id",checkFrappeAuth,getGuestsByHost)
 
-// GuestGroup   
-router.post('/createGroup',checkFrappeAuth,createGroup)
+// GuestGroup    
 router.put("/addEventToGroup/:group_id",checkFrappeAuth,addEventToGroup)
 router.get("/getGroupsByOrder/:order_id",checkFrappeAuth,getGroupsByOrder)
 router.delete("/removeEventFromGroup/:group_id/:eventId",checkFrappeAuth,removeEventFromGroup)
@@ -43,7 +42,7 @@ router.post('/addGuestsToGroup',checkFrappeAuth,addGuestsToGroup)
 router.delete("/removeGuestFromGroup/:relation_id",checkFrappeAuth,removeGuestFromGroup)
 router.get("/getHostGroupGuests/:group_id",checkFrappeAuth,getHostGroupGuests)
 router.get("/invitedGuest/:order_id",checkFrappeAuth,invitedGuest)
-router.post("/moveMultipleGuests",checkFrappeAuth,moveGuestToNewGroup)
+router.post("/moveGuestToNewGroup",checkFrappeAuth,moveGuestToNewGroup)
 router.get("/getAvailableGuestsByOrder/:host_id/:order_id",checkFrappeAuth,getAvailableGuestsByOrder)
 
 // Msg FOrmate Api
