968c9b3f37285b0c9f7d4452eff14914d7c001b3
dev <dev@klickinvite.com>
2025-10-15 16:00:01 +0530

    change the api route

--- src/routes/template.routes.js
+++ src/routes/template.routes.js
@@ -58,7 +58,7 @@ router.delete("/deleteMsgFormat/:msg_id",checkFrappeAuth,deleteMsgFormat)
 // Invitaions APi
 router.post("/sendInvitation",checkFrappeAuth,sendInvitation)
 router.get("/getGroupById/:id",checkFrappeAuth,getGroupById)
-router.get("/openInvite/:",openInvite)
+router.get("/openInvite/:uniqueUrl",openInvite)
 
 // RSVP API
 router.post("/submitRSVP",checkFrappeAuth,submitRSVP)
